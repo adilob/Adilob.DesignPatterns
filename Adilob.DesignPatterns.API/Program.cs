16ff64cd662b24339b456b62cc7bcfe29b65575c
Adilo Bertoncello <adilo.bertoncello@birdirx.com>
2024-12-15 16:53:17 -0300

    debug proxy using http file added services to program.cs

--- Adilob.DesignPatterns.API/Program.cs
+++ Adilob.DesignPatterns.API/Program.cs
@@ -1,4 +1,14 @@
 
+using Adilob.DesignPatterns.API.Application.ChainOfResponsability;
+using Adilob.DesignPatterns.API.Infrastructure.Customers;
+using Adilob.DesignPatterns.API.Infrastructure.Deliveries;
+using Adilob.DesignPatterns.API.Infrastructure.Integrations;
+using Adilob.DesignPatterns.API.Infrastructure.Orders;
+using Adilob.DesignPatterns.API.Infrastructure.Payments.Services;
+using Adilob.DesignPatterns.API.Infrastructure.Payments;
+using Adilob.DesignPatterns.API.Infrastructure.Products;
+using Adilob.DesignPatterns.API.Infrastructure.Proxies;
+
 namespace Adilob.DesignPatterns.API
 {
 	public class Program
@@ -8,6 +18,31 @@ namespace Adilob.DesignPatterns.API
 			var builder = WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
+			builder.Services.AddMemoryCache();
+			builder.Services.AddHttpContextAccessor();
+
+			builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+			builder.Services.AddScoped<IProductRepository, ProductRepository>();
+			builder.Services.AddScoped<IPaymentFraudCheckService, PaymentFraudCheckService>();
+			builder.Services.AddScoped<IOrderHandler, CheckForFraudHandler>();
+			builder.Services.AddScoped<IOrderHandler, ValidateCustomerHandler>();
+			builder.Services.AddScoped<IOrderHandler, ValidateStockHandler>();
+			builder.Services.AddScoped<IPostOrderHandler, PostOrderHandler>();
+			builder.Services.AddScoped<CustomerRepositoryProxy>();
+
+			builder.Services.AddTransient<IPaymentService, CreditCardService>();
+			builder.Services.AddTransient<IPaymentService, PaymentSlipService>();
+			builder.Services.AddTransient<IExternalPaymentSlipService, ExternalPaymentSlipService>();
+			builder.Services.AddTransient<ICoreCrmIntegrationService, CoreCrmIntegrationService>();
+			builder.Services.AddTransient<IAntiFraudFacade, AntiFraudFacade>();
+			builder.Services.AddTransient<IDeliveryService, NationalDeliveryService>();
+			builder.Services.AddTransient<IDeliveryService, InternationalDeliveryService>();
+			builder.Services.AddTransient<IPaymentServiceFactory, PaymentServiceFactory>();
+			builder.Services.AddTransient<IOrderAbstractFactory, NationalOrderAbstractFactory>();
+			builder.Services.AddTransient<IOrderAbstractFactory, InternationalOrderAbstractFactory>();
+
+			builder.Services.AddSingleton<IOrderFactorySelector, OrderFactorySelector>();
+			builder.Services.AddSingleton<PaymentMethodsFactory>();
 
 			builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
